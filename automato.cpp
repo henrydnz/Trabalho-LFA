898c3f8136c306e835382f545b65f02e9768bae4
henrydnz <riquednz@gmail.com>
2025-07-07 15:53:26 -0300

    implementacao de print_step, corr de logica das demais, comentarios

--- automato.cpp
+++ automato.cpp
@@ -1,25 +1,47 @@
 #include "automato.h"
 
+void print_step(state state_curr, std::string s, int step){
+    std::cout << "[q" << state_curr.id << "]";
+    
+    for(int i = step; i < s.size(); i++)
+        std::cout << s[i];
+
+    std::cout << std::endl;
+}
+
 int return_target_id(state s, char read_curr){
+    //passa pelas transicoes do estado e retorna o id 
+    //do estado de destino
     for(transition t : s.trans)
         if(t.read == read_curr)
             return t.target;
-    return s.id;
+    //se nao tem o caractere em nenhuma transicao entao
+    //a palavra ja e invalida
+    return -1;
 }
 
-void print_step(state state_curr, std::string s, int i){
-    //implementar
-}
+int valid(automata a, std::string s){
+    //se string vazia, q0 deve ser final
+    if(s[0] == '@')
+        return a[0].is_final;
 
-
-void step(automata a, std::string s){
     state state_curr;
-    int step = 0, target_id;
-    char read_curr;
-    for(int i = 0; i < s.size(); i++){
-        read_curr = s[i];
-        target_id = return_target_id(state_curr, read_curr);
+    int i, target_id;
+
+    //roda por cada char da string, avança pra próxima transição
+    for(i = 0; i < s.size(); i++){
+        target_id = return_target_id(state_curr, s[i]);
+        
+        //sem transicao, palavra rejeitada
+        if(target_id == -1) 
+            return false;
+
         state_curr = a[target_id];
-        print_step(state_curr, s, i); //implementar
+        print_step(state_curr, s, i);
     }
+
+    //se rodou por toda a string entao testa se o ultimo
+    //estado é final
+    return state_curr.is_final;
 }
+
